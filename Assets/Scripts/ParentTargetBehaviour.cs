e2b6aa874d74d699602d0eacb5914bd8ba64cc5b
hazemahmed <hazemahmed45@gmail.com>
2018-08-12 11:25:23 +0200

    full code

---
+++ Assets/Scripts/ParentTargetBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParentTargetBehaviour : MonoBehaviour {
+
+	Vector3 leftPos,rightPos;
+	float xRange=10f;
+	float lerpValue=1f;
+	bool lerp=false;
+	PlayerShootBehaviour playerShootBehaviour;
+	void Start()
+	{
+		playerShootBehaviour=GameObject.FindGameObjectWithTag("GameController").GetComponent<PlayerShootBehaviour>();
+		leftPos=new Vector3(transform.position.x-xRange,transform.position.y,transform.position.z);
+		rightPos=new Vector3(transform.position.x+xRange,transform.position.y,transform.position.z);
+	}
+
+	void Update()
+	{
+		if(playerShootBehaviour.score>=1000)
+		{
+
+			if(lerp)
+			{
+				transform.position=Vector3.Lerp(transform.position,leftPos,lerpValue*Time.deltaTime);
+				if(transform.position.x<=leftPos.x+0.5f)
+				{
+					lerp=false;
+				}
+			}
+			else
+			{
+				transform.position=Vector3.Lerp(transform.position,rightPos,lerpValue*Time.deltaTime);
+				if(transform.position.x>=rightPos.x-0.5f)
+				{
+					lerp=true;
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerShootBehaviour.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerShootBehaviour : MonoBehaviour {
+
+	public GameObject bullet,bulletRespawn;
+	public LayerMask layerMask;
+	// Use this for initialization
+	public Text scoreText;
+	public int score;
+	public AudioSource gunShotAudio;
+	void Start()
+	{
+		score=0;
+	}
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Mouse0))
+		{
+			//Instantiate(bullet,bulletRespawn.transform.position,bulletRespawn.transform.rotation);
+			if(score>3000)
+			{
+				scoreText.text="YOU WON";
+			}
+			else
+			{
+				RaycastHit hit;
+				gunShotAudio.Play();
+				if(Physics.Raycast(bulletRespawn.transform.position,bulletRespawn.transform.forward,out hit,1000f,layerMask))
+				{
+					
+					GameObject bullObject= Instantiate(bullet,hit.point,hit.collider.transform.rotation);
+					StartCoroutine(waitForBulletToDestroy(bullObject));
+					GameObject target=hit.collider.gameObject;
+					score +=target.GetComponent<TargetBehaviour>().targetScore;
+					scoreText.text="your score: "+score;
+					Debug.Log("Score increased by "+score);
+					//Debug.Log(hit.collider.name+" "+hit.collider.transform.position);
+				
+				}
+			}
+		}
+		//or
+		// if(Input.GetMouseButtonDown(0))
+		// {
+		// 	Debug.Log("HERE");
+		// }
+	}
+	IEnumerator waitForBulletToDestroy(GameObject bull)
+	{
+		yield return new WaitForSeconds(5);
+		Destroy(bull);
+	}
+}
